2a74ed6ed7e060878ac3544d7763fd3124f44fe1
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-17 14:18:39 -0300

    Base terminada :)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
-}
+  
+  let x: number = 0;
+  let retornar: string = "";
+  while(x<cantidad){
+      retornar= retornar + letra; 
+    x++;
+  }
+ return retornar;
+  }
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+let n: number = 0;
+
+while (n<tamaño){
+  console.log(repetirLetra(caracter, tamaño))
+  n++;
+}
+
