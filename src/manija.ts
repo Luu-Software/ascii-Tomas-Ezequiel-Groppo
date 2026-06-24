280749a54a1f280050769261abb7611dc76ae653
Tomas-Ezequiel-Groppo <tommygroppo70@gmail.com>
2026-06-24 15:32:10 -0300

    Manija en proceso (Ya se pueden hacer triángulos rectángulos)

--- src/manija.ts
+++ src/manija.ts
@@ -14,31 +14,19 @@ function filaTrianguloIsoceles(letra: string, fila: number, tamaño: number): st
   // COMPLETAR
   /* fila n: 2*n+1 asteriscos, con espacios a izquierda a derecha 
   Pueden asumir que el tamaño es impar, así el triángulo queda centrado. */
-  return "tumadre"
+  return "era un placeholder"
 }
 let caracter: string = preguntar('Con que caracter vas a dibujar el triangulo? ');
 let tamaño: number = Number(preguntar('De que tamaño? '));
 let tipo: string = preguntar('Que tipo de triangulo? (isoceles, rectangulo) ');
 let n: number = 0;
-let cantidadporfila: string;
 
-if (tipo==="isoceles" || tipo==="Isoceles"){
-
-}
-else if (tipo==="rectangulo" || tipo==="Rectangulo"){
-while (n<tamaño){
-  if (tamaño=1){
-    cantidadporfila=caracter
-    console.log(cantidadporfila + " ")
+if (tipo==="Rectangulo" || tipo==="rectangulo"){
+  while (n<tamaño+1){
+    console.log(repetirLetra(caracter, n))
     n++;
-  }
-  else{
-    cantidadporfila=caracter+caracter
-    console.log(cantidadporfila + " ")
-    n ++;
-  }
   }
 }
 else{
-  console.log("Ingresa un tipo de triangulo valido")
+  console.log ("hola")
 }
